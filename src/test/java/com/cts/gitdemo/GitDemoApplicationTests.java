ca14ce635ffdfe227e8e8006529247781477b442
jatinnamdev24 <jatinnamdev24@gmail.com>
2020-12-16 09:32:27 +0530

    first commit

---
+++ src/test/java/com/cts/gitdemo/GitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.gitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
